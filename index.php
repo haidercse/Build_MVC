8eb915aaed4cbef112ddfec807871853f39b7fde
haider-cse <haider.cse7644@gmail.com>
2022-03-10 17:41:19 -0800

    trying to create dynamic route

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-<?php 
-   echo "Hello world!";
+<?php
+
+use app\core\Application;
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$app = new Application;
+
+
+$app->router->get('/', function () {
+   return "Hello World!";
+});
+
+$app->router->get('/contact', function () {
+   return "Hello World! contact again";
+});
+
+
+
+$app->run();
 
 ?>
