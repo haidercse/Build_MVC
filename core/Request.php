82c8ef47377864d588366ca87d6827240a5f1f78
haider-cse <haider.cse7644@gmail.com>
2022-03-10 21:52:18 -0800

    add

--- core/Request.php
+++ core/Request.php
@@ -8,6 +8,10 @@ class Request
     public function getPath()
     {     
         $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $position = strpos($path, '?');
+        echo "<pre>";
+        var_dump($position);
+        echo "</pre>";
     }
 
     public function getMethod()
--- core/Router.php
+++ core/Router.php
@@ -4,16 +4,19 @@ namespace app\core;
 
 class Router
 {
+    public Request $request;
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
     protected array $routes = [];
     public function get($path, $callback){
      
            $this->routes['get'][$path] = $callback; 
+         
     }
 
     public function resolve(){
-        echo "<pre>";
-        var_dump($_SERVER);
-        echo "<pre>";
-        exit;
+       $this->request->getPath();
     }
 }
